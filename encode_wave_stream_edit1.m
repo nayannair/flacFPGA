05c98e20231e10c68ecb1dc6350f63bcf86c2e85
mayurvbc <32449777+mayurvbc@users.noreply.github.com>
2019-11-20 10:33:12 +0530

    Update encode_wave_stream_edit1.m

--- encode_wave_stream_edit1.m
+++ encode_wave_stream_edit1.m
@@ -1,17 +1,29 @@
 function y = encode_wave_stream_edit1(wave_stream)
 frames = []; %Define a list of frames
+BLOCK_SIZE      = 4096;  %# Num samples per block
+SAMPLE_RATE     = 44100; %# Hz
+SAMPLE_SIZE     = 16    ;%# Num bits per sample
+NUM_CHANNELS    = 2;
 
-    for sample_index= 1:BLOCK_SIZE:wave_stream.num_samples %Feed Block size, num_samples
+    [y,Fs] = audioread(input);
+    sample_size = SAMPLE_SIZE ; %input_file.getsampwidth() * 8          %   # Convert bytes per sample into bits per sample
+    sample_rate = SAMPLE_RATE ;%input_file.getframerate()               %  # In Hz
+    num_channels = NUM_CHANNELS;%input_file.getnchannels()
+    num_samples = length(y);%input_file.getnframes()
+    num_interleaved_samples = num_channels * num_samples;
+       % md5_digest = hashlib.md5(raw_frames).digest()
+       
+    for sample_index= 1:BLOCK_SIZE:num_samples %Feed Block size, num_samples
         frame_number = floor((sample_index-1) / BLOCK_SIZE);
         %sample_index-1 is put here to convert MATLAB 1 to python 0
         subframes = []; %Define a list of subframes
 
-        for chan_ind= 1:wave_stream.num_channels %Iterate over different channel
+        for chan_ind= 1:num_channels %Iterate over different channel
             
            %subframe_candidates.append(make_subframe_constant(channel, sample_index))
            %subframe_candidates.append(make_subframe_verbatim(channel, sample_index))
             for fixed_predictor_order = 1:MAX_FIXED_PREDICTOR_ORDER
-                subframe_sizes = length(make_subframe_fixed(wave_stream.channel(chan_ind), sample_index-1, fixed_predictor_order));% Pass each channel into this
+                subframe_sizes = length(make_subframe_fixed(y(:,chan_ind), sample_index-1, fixed_predictor_order));% Pass each channel into this
             end
              
             % subframe_candidates = filter(None, subframe_candidates) Use
@@ -21,7 +33,7 @@ frames = []; %Define a list of frames
             subframes = [subframes smallest_subframe]; %Add to list of subframes
         end
         if (wave_stream.num_samples - sample_index-1) < BLOCK_SIZE 
-            num_samples_in_frame = (wave_stream.num_samples - sample_index);
+            num_samples_in_frame = (num_samples - sample_index);
         else
             num_samples_in_frame = BLOCK_SIZE;
         end
@@ -30,12 +42,13 @@ frames = []; %Define a list of frames
         frames = [frames frame];
     end
     
-    metadata_block_stream_info = MetadataBlockStreamInfo(wave_stream.num_samples, wave_stream.md5_digest);
-    metadata_block_header = MetadataBlockHeader(True, BLOCK_TYPE_STREAMINFO, length(metadata_block_stream_info));
+    metadata_block_stream_info = MetadataBlockStreamInfo(num_samples, wave_stream.md5_digest);
+    metadata_block_header = MetadataBlockHeader( BLOCK_TYPE_STREAMINFO, length(metadata_block_stream_info));
     metadata_block = MetadataBlock(metadata_block_header, metadata_block_stream_info);
 
     metadata_blocks = metadata_block;
 
-    stream = ['fLaC' metadata_blocks frames];
+    stream = ['01100110' '01001100' '01100001' '01000011'  metadata_blocks frames];
 
     y = stream;
+end
