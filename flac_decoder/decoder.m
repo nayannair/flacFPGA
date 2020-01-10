dd33af797962bf9454e4f900355f4dd8bfbffc0f
mayurvbc <32449777+mayurvbc@users.noreply.github.com>
2020-01-10 18:56:42 +0530

    Update decoder.m

--- flac_decoder/decoder.m
+++ flac_decoder/decoder.m
@@ -1,4 +1,4 @@
-function [y,j] = decoder(input_flac)
+function [y] = decoder(input_flac)
  id = fopen(input_flac,'r');
  input_data=[];
  input_data= fscanf(id,'%s');
@@ -48,6 +48,58 @@ function [y,j] = decoder(input_flac)
      md5 = j(1,counter:counter+127);
      counter = counter+128;
      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     %Frame
+     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+     counter=counter+32;
+        %to skip utf8 bits
+         temp= j(1,counter:counter+7);
+         temp1 =bin2dec(temp);
+         var = bin2dec('11111111') %0xFF
+            while (temp1>=bin2dec('11000000'))
+                counter = counter+8;
+                temp1 = bitand(bitsll(1,temp1),var);
+            end
+      counter = counter +8;% skip 8 crc bits
+     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     %subframe
+     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    counter = counter+8;% 6 bits are for switching subframe type
+    result = j(1,counter:counter+63);% 64 = 16 * 4 
+    counter = counter + 64;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %rice decode
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    residual_signal = [];
+    counter = counter +2;% residual coding method
+    counter = counter +4;%partition order
+    parameter = j(1,counter:counter+4);%5 bits for rice parameter
+    num_samples_part = 4092;
+    high_order_bits =0;
+    low_order_bits =0;
+        while num_samples_part >0
+            num_samples_part= num_samples_part-1;
+            while j(1,counter) == 0
+                counter = counter +1;
+                high_order_bits =high_order_bits+1; 
+            end    
+                high_order_bits = dec2bin(high_order_bits);
+                counter = counter +1;
+                low_order_bits =j(1,counter:counter+parameter -1);
+                low_order_bits= num2str(low_order_bits);
+                low_order_bits(isspace(low_order_bits)) = '';
+                sample_bits = [high_order_bits low_order_bits];
+                mapped_sample = bin2dec(sample_bits);
+                
+                if(rem(mapped_sample,2)==0)
+                    sample = mapped_sample/2;
+                else 
+                    sample = -((mapped_sample+1)/2);
+                    
+                end
+                residual_signal = [residual_signal sample];
+        end
+                
  end
  
  %min_blk_size = input_data(1,1:4)
